b1b4790bb0e9af53e74ef861168ad05f46364879
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-09 04:41:19 +0200

    gulp dev server tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,28 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+gulp.task('html', function () {
+  gulp.src('src/*.html')
+    .pipe(connect.reload());
 });
 
-gulp.task('serve', function() {
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('server', function() {
   connect.server({
     livereload: true,
     root: 'src',
     port: 9001,
     https: false
   });
+});
+
+gulp.task('watch', ['server', 'sass'], function (){
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/*.html', ['html']);
 });
