4bd9f69dc5cfc475d74a5f0bc35975f8a782a518
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-09 04:08:31 +0200

    add gulp serve basic task

---
+++ README.md
@@ -0,0 +1,2 @@
+# at-flex
+A lightweight CSS3 flexbox grid with extra flex functionalities.
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
+/*!
+* Author: Ahmed Tokyo
+* ahmed.tokyo1@gmail.com
+* tokyo@dreidev.com
+*/
 var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('serve', function() {
+  connect.server({
+    livereload: true,
+    root: 'src',
+    port: 9001,
+    https: false
+  });
 });
